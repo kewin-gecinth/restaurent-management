f9f7bbff74164744af790a54e9b3703fc78477ad
kewin-gecinth <120256560+kewin-gecinth@users.noreply.github.com>
2022-12-12 09:33:13 +0400

    Create DatabaseException.java

---
+++ src/main/java/DatabaseException.java
@@ -0,0 +1,11 @@
+public class DatabaseException extends Exception {
+    public String errMsg;
+
+    public DatabaseException(String msg) {
+        errMsg = msg;
+    }
+
+    public String getErrMessage() {
+        return errMsg;
+    }
+}
